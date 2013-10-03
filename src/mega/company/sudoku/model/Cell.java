8cef499ee3a411845f138430efc6908736873639
JOSLE <mironovga@gmail.com>
2013-10-03 22:28:07 +0300

    model commit
    josle

--- src/mega/company/sudoku/model/Cell.java
+++ src/mega/company/sudoku/model/Cell.java
@@ -1,5 +1,13 @@
 package mega.company.sudoku.model;
 
 public class Cell {
+	protected int value;
 
+	public int getValue() {
+		return value;
+	}
+	
+	public Cell(int _value) {
+		value = _value;
+	}
 }
--- src/mega/company/sudoku/model/Field.java
+++ src/mega/company/sudoku/model/Field.java
@@ -1,5 +1,29 @@
 package mega.company.sudoku.model;
 
 public class Field {
-
+	protected Cell[][] field;
+	protected int size;
+	
+	public Cell[][] getField() {
+		return field;
+	}
+	
+	public Field(int[][] _matrix) {
+		size = 9;
+		field = new Cell[size][size];
+		
+		for (int i = 0; i < size; i++)
+			for (int j = 0; j < size; j++)
+				field[i][j] = new Cell(_matrix[i][j]);
+	}
+	
+	public int[][] getIntMatrix() {
+		int[][] temp = new int[size][size];
+		
+		for (int i = 0; i < size; i++)
+			for (int j = 0; j < size; j++)
+				temp[i][j] = field[i][j].getValue();
+		
+		return temp;				
+	}
 }
